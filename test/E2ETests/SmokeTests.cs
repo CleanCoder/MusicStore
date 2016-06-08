77c0dc5391a6b6c19b9e0084bddcf7ec7cf0bd0c
= <jt.luo@mail.utoronto.ca>
2016-06-08 15:01:23 -0700

    Add eventlistener

--- test/E2ETests/SmokeTests.cs
+++ test/E2ETests/SmokeTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -111,8 +112,11 @@ namespace E2ETests
             ApplicationType applicationType,
             string applicationBaseUrl)
         {
-            var smokeTestRunner = new SmokeTests(_logger);
-            await smokeTestRunner.SmokeTestSuite(serverType, runtimeFlavor, architecture, applicationType, applicationBaseUrl);
+            using (new ConsoleEventListener("System-Net"))
+            {
+                var smokeTestRunner = new SmokeTests(_logger);
+                await smokeTestRunner.SmokeTestSuite(serverType, runtimeFlavor, architecture, applicationType, applicationBaseUrl);
+            }
         }
         public void Dispose()
         {
@@ -205,6 +209,46 @@ namespace E2ETests
                     Helpers.SetInMemoryStoreForIIS(deploymentParameters, _logger);
 
                     await SmokeTestHelper.RunTestsAsync(deploymentResult, _logger);
+                }
+            }
+        }
+    }
+
+    public class ConsoleEventListener : EventListener
+    {
+        private readonly string _eventFilter;
+
+        public ConsoleEventListener() : this(string.Empty) { }
+
+        public ConsoleEventListener(string filter)
+        {
+            if (filter == null)
+                throw new ArgumentNullException(nameof(filter));
+
+            _eventFilter = filter;
+
+            foreach (EventSource source in EventSource.GetSources())
+                EnableEvents(source, EventLevel.LogAlways);
+        }
+
+        protected override void OnEventSourceCreated(EventSource eventSource)
+        {
+            base.OnEventSourceCreated(eventSource);
+            EnableEvents(eventSource, EventLevel.LogAlways, EventKeywords.All);
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            lock (Console.Out)
+            {
+                string text = $"[{eventData.EventSource.Name}-{eventData.EventId}]{(eventData.Payload != null ? $" ({string.Join(", ", eventData.Payload)})." : "")}";
+                
+                if (_eventFilter != null && text.Contains(_eventFilter))
+                {
+                    ConsoleColor origForeground = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(text);
+                    Console.ForegroundColor = origForeground;
                 }
             }
         }
